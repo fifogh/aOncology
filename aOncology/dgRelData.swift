6626bf3c6aa71cec0762d5365a61307def71b761
Philippe Faurie <EasyDialFaurie@Philippes-iMac.local>
2018-04-24 20:40:46 -0700

    add data file

---
+++ aOncology/dgRelData.swift
@@ -0,0 +1,947 @@
+var dicGDL =
+[ "ABCB1" :
+    [ "docetaxel",
+      "doxorubicin",
+      "epirubicin",
+      "paclitaxel" ],
+ "ABL1" :
+    [ "alectinib",
+      "axitinib",
+      "bosutinib",
+      "dasatinib",
+      "ibrutinib",
+      "imatinib",
+      "nilotinib",
+      "ponatinib",
+      "regorafenib",
+      "brigatinib" ],
+ "ABL2" :
+    [ "axitinib",
+      "bosutinib",
+      "ceritinib",
+      "dasatinib",
+      "ibrutinib",
+      "imatinib",
+      "nilotinib",
+      "ponatinib",
+      "regorafenib" ],
+ "ADRA1A" :
+    [ "alfuzosin" ],
+ "ADRA1B" :
+    [ "alfuzosin" ],
+ "ADRA1D" :
+    [ "alfuzosin" ],
+ "ADRA1x" :
+    [ "alfuzosin" ],
+ "ALK" :
+    [ "alectinib",
+      "ceritinib",
+      "crizotinib",
+      "brigatinib" ],
+ "ARAF" :
+    [ "vemurafenib" ],
+ "AURKA" :
+    [ "axitinib",
+      "ceritinib",
+      "brigatinib" ],
+ "AXL" :
+    [ "axitinib",
+      "cabozantinib",
+      "ceritinib" ],
+ "B4GALNT1" :
+    [ "dinutuximab" ],
+ "BCL2" :
+    [ "venetoclax" ],
+ "BCL2L1" :
+    [ "venetoclax" ],
+ "BCL2L2" :
+    [ "venetoclax" ],
+ "BCR-abl" :
+    [ "bosutinib",
+      "dasatinib",
+      "imatinib",
+      "nilotinib",
+      "ponatinib" ],
+ "BIRC5" :
+    [ "fluorouracil" ],
+ "BLK" :
+    [ "ibrutinib",
+      "osimertinib",
+      "ponatinib",
+      "vandetanib",
+      "vemurafenib",
+      "brigatinib" ],
+ "BMX" :
+    [ "ibrutinib" ],
+ "BRAF" :
+    [ "dabrafenib",
+      "regorafenib",
+      "sorafenib",
+      "vemurafenib" ],
+ "BRSK1" :
+    [ "brigatinib" ],
+ "BRSK2" :
+    [ "brigatinib" ],
+ "BTK" :
+    [ "ibrutinib",
+      "brigatinib" ],
+ "C5" :
+    [ "eculizumab" ],
+ "CA9" :
+    [ "bevacizumab",
+      "fluorouracil" ],
+ "CAMK2G" :
+    [ "brigatinib" ],
+ "CAMK2N2" :
+    [ "brigatinib" ],
+ "CaMKK2" :
+    [ "brigatinib" ],
+ "CD19" :
+    [ "blinatumomab" ],
+ "CD22" :
+    [ "inotuzumab ozogamicin" ],
+ "CD274" :
+    [ "atezolizumab",
+      "durvalumab" ],
+ "CD3" :
+    [ "blinatumomab" ],
+ "CD33" :
+    [ "gemtuzumab ozogamicin",
+      "muronomab-CD3" ],
+ "CD38" :
+    [ "daratumumab" ],
+ "CD52" :
+    [ "alemtuzumab" ],
+ "CDK1" :
+    [ "midostaurin",
+      "abemaciclib" ],
+ "CDK4" :
+    [ "palbociclib",
+      "ribociclib",
+      "abemaciclib" ],
+ "CDK6" :
+    [ "palbociclib",
+      "ribociclib",
+      "abemaciclib" ],
+ "CDK9" :
+    [ "abemaciclib" ],
+ "CHEK1" :
+    [ "brigatinib" ],
+ "CHEK2" :
+    [ "brigatinib" ],
+ "CLK1" :
+    [ "brigatinib" ],
+ "CLK2" :
+    [ "brigatinib" ],
+ "CSF1R" :
+    [ "axitinib",
+      "imatinib",
+      "nilotinib",
+      "pazopanib",
+      "sunitinib",
+      "brigatinib" ],
+ "CSK" :
+    [ "ibrutinib" ],
+ "CTLA4" :
+    [ "ipilimumab" ],
+ "CXCL11" :
+    [ "rolapitant" ],
+ "CXCR4" :
+    [ "plerixafor" ],
+ "CYP17A1" :
+    [ "abiraterone" ],
+ "CYP19A1" :
+    [ "anastrozole",
+      "exemestane",
+      "letrozole",
+      "testolactone" ],
+ "DDR1" :
+    [ "nilotinib" ],
+ "DDR2" :
+    [ "regorafenib" ],
+ "DHFR" :
+    [ "methotrexate",
+      "pemetrexed",
+      "pralatrexate" ],
+ "DNA" :
+    [ "cyclophosphamide",
+      "gemtuzumab ozogamicin",
+      "mechlorethamine" ],
+ "DNA damage" :
+    [ "niraparib",
+      "rucaparib",
+      "olaparib",
+      "mitomycin",
+      "carboplatin",
+      "oxaliplatin",
+      "cisplatin" ],
+ "DNMT1" :
+    [ "azacitidine",
+      "decitabine" ],
+ "DNMT3A" :
+    [ "azacitidine",
+      "decitabine" ],
+ "DNMT3B" :
+    [ "azacitidine",
+      "decitabine" ],
+ "EDNRA" :
+    [ "macitentan" ],
+ "EDNRB" :
+    [ "macitentan" ],
+ "EGFR" :
+    [ "afatinib",
+      "ceritinib",
+      "cetuximab",
+      "erlotinib",
+      "gefitinib",
+      "ibrutinib",
+      "lapatinib",
+      "necitumumab",
+      "panitumumab",
+      "vandetanib",
+      "neratinib",
+      "brigatinib" ],
+ "EPHA2" :
+    [ "dasatinib",
+      "regorafenib" ],
+ "EPHA3" :
+    [ "ponatinib",
+      "vandetanib" ],
+ "EPHA5" :
+    [ "ponatinib",
+      "vandetanib" ],
+ "EPHA7" :
+    [ "ponatinib",
+      "vandetanib" ],
+ "EPHB1" :
+    [ "ponatinib",
+      "vandetanib" ],
+ "EPHx" :
+    [ "ponatinib",
+      "vandetanib" ],
+ "ERBB2" :
+    [ "ado-trastuzumab emtansine",
+      "afatinib",
+      "ibrutinib",
+      "lapatinib",
+      "osimertinib",
+      "pertuzumab",
+      "trastuzumab",
+      "neratinib",
+      "brigatinib" ],
+ "ERBB4" :
+    [ "afatinib",
+      "lapatinib",
+      "osimertinib",
+      "brigatinib" ],
+ "EZH2" :
+    [ "tamoxifen" ],
+ "F2R" :
+    [ "vorapaxar" ],
+ "FER" :
+    [ "brigatinib" ],
+ "FES" :
+    [ "brigatinib" ],
+ "FGFR1" :
+    [ "axitinib",
+      "lenvatinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "regorafenib",
+      "sorafenib",
+      "sunitinib",
+      "brigatinib" ],
+ "FGFR2" :
+    [ "ceritinib",
+      "lenvatinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "regorafenib",
+      "brigatinib" ],
+ "FGFR3" :
+    [ "axitinib",
+      "ceritinib",
+      "lenvatinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "brigatinib" ],
+ "FGFR4" :
+    [ "ceritinib",
+      "lenvatinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "brigatinib" ],
+ "FGR" :
+    [ "ibrutinib",
+      "ponatinib",
+      "vandetanib",
+      "vemurafenib",
+      "brigatinib",
+      "midostaurin" ],
+ "FKBP1A" :
+    [ "everolimus",
+      "sirolimus",
+      "temsirolimus" ],
+ "FLT1" :
+    [ "axitinib",
+      "cabozantinib",
+      "lenvatinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "regorafenib",
+      "sorafenib",
+      "sunitinib",
+      "vandetanib",
+      "midostaurin" ],
+ "FLT3" :
+    [ "axitinib",
+      "cabozantinib",
+      "ceritinib",
+      "ibrutinib",
+      "nintedanib",
+      "ponatinib",
+      "sorafenib",
+      "sunitinib",
+      "vandetanib",
+      "brigatinib",
+      "midostaurin" ],
+ "FLT4" :
+    [ "axitinib",
+      "cabozantinib",
+      "lenvatinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "regorafenib",
+      "sorafenib",
+      "vandetanib",
+      "brigatinib" ],
+ "FRK" :
+    [ "ibrutinib",
+      "ponatinib",
+      "vandetanib",
+      "brigatinib" ],
+ "FUS" :
+    [ "trabectidin" ],
+ "FYN" :
+    [ "dasatinib",
+      "ibrutinib",
+      "ponatinib",
+      "vandetanib",
+      "brigatinib" ],
+ "GD2" :
+    [ "dinutuximab" ],
+ "GLI1" :
+    [ "arsenic trioxide" ],
+ "GLI2" :
+    [ "arsenic trioxide" ],
+ "GNRHR" :
+    [ "abarelix",
+      "degarelix",
+      "goserelin",
+      "histrelin",
+      "leuprolide",
+      "triptorelin" ],
+ "HCK" :
+    [ "bosutinib",
+      "ibrutinib",
+      "ponatinib",
+      "vandetanib",
+      "brigatinib" ],
+ "HDAC1" :
+    [ "belinostat",
+      "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC10" :
+    [ "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC11" :
+    [ "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC2" :
+    [ "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC3" :
+    [ "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC4" :
+    [ "belinostat",
+      "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC5" :
+    [ "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC6" :
+    [ "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC7" :
+    [ "belinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDAC8" :
+    [ "romidepsin",
+      "vorinostat" ],
+ "HDAC9" :
+    [ "panobinostat",
+      "romidepsin",
+      "vorinostat" ],
+ "HDACx" :
+    [ "belinostat",
+      "vorinostat" ],
+ "HPD" :
+    [ "nitisinone" ],
+ "HTR3A" :
+    [ "dolastron",
+      "granisetron",
+      "ondansetron",
+      "palonosetron" ],
+ "HTR3B" :
+    [ "dolastron",
+      "granisetron",
+      "ondansetron",
+      "palonosetron" ],
+ "HTR3C" :
+    [ "dolastron",
+      "granisetron",
+      "ondansetron",
+      "palonosetron" ],
+ "HTR3D" :
+    [ "dolastron",
+      "granisetron",
+      "ondansetron",
+      "palonosetron" ],
+ "IDH2" :
+    [ "enasidenib" ],
+ "IGF1R" :
+    [ "ceritinib",
+      "brigatinib" ],
+ "IL12B" :
+    [ "ustekinumab" ],
+ "IL17A" :
+    [ "ixekizumab",
+      "secukinumab" ],
+ "IL17RA" :
+    [ "brodalumab" ],
+ "IL1B" :
+    [ "canakinumab" ],
+ "IL1R1" :
+    [ "anakinra" ],
+ "IL1R2" :
+    [ "anakinra" ],
+ "IL23" :
+    [ "ustekinumab" ],
+ "IL2RA" :
+    [ "basiliximab",
+      "daclizumab" ],
+ "IL4RA" :
+    [ "dupilumab" ],
+ "IL5" :
+    [ "mepolizumab",
+      "reslizumab" ],
+ "IL6" :
+    [ "pirfenidone",
+      "siltuximab" ],
+ "IL6R" :
+    [ "tocilizumab" ],
+ "INFG" :
+    [ "pirfenidone" ],
+ "INSR" :
+    [ "alectinib",
+      "ceritinib",
+      "brigatinib" ],
+ "INSRR" :
+    [ "brigatinib" ],
+ "ITGA2B" :
+    [ "abciximab" ],
+ "ITGA4" :
+    [ "natalizumab" ],
+ "ITGAL" :
+    [ "efalizumab" ],
+ "ITK" :
+    [ "ibrutinib",
+      "pazopanib" ],
+ "JAK1" :
+    [ "ruxolitinib",
+      "tofacitinib" ],
+ "JAK2" :
+    [ "ceritinib",
+      "ruxolitinib",
+      "tofacitinib",
+      "brigatinib" ],
+ "JAK3" :
+    [ "ibrutinib",
+      "ruxolitinib",
+      "tofacitinib" ],
+ "KDR" :
+    [ "axitinib",
+      "cabozantinib",
+      "ceritinib",
+      "lenvatinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "ramucirumab",
+      "regorafenib",
+      "sorafenib",
+      "sunitinib",
+      "vandetanib",
+      "midostaurin",
+      "midostaurin" ],
+ "KIT" :
+    [ "axitinib",
+      "cabozantinib",
+      "dasatinib",
+      "imatinib",
+      "lenvatinib",
+      "nilotinib",
+      "pazopanib",
+      "ponatinib",
+      "regorafenib",
+      "sorafenib",
+      "sunitinib",
+      "brigatinib" ],
+ "KLKB1" :
+    [ "midostaurin" ],
+ "LCK" :
+    [ "ceritinib",
+      "dasatinib",
+      "ibrutinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "vandetanib",
+      "vemurafenib",
+      "brigatinib" ],
+ "LRRK2" :
+    [ "brigatinib" ],
+ "LTK" :
+    [ "brigatinib" ],
+ "LYMK1" :
+    [ "dabrafenib" ],
+ "LYN" :
+    [ "bosutinib",
+      "ceritinib",
+      "dasatinib",
+      "ibrutinib",
+      "nintedanib",
+      "ponatinib",
+      "vandetanib",
+      "vemurafenib",
+      "brigatinib" ],
+ "MAP2K1" :
+    [ "cobimetinib",
+      "trametinib" ],
+ "MAP2K2" :
+    [ "cobimetinib",
+      "trametinib" ],
+ "MAP3K9" :
+    [ "brigatinib" ],
+ "MAP4K5" :
+    [ "vemurafenib" ],
+ "MAPK14" :
+    [ "bosutinib" ],
+ "MARK1" :
+    [ "brigatinib" ],
+ "MARK2" :
+    [ "brigatinib" ],
+ "MARK3" :
+    [ "brigatinib" ],
+ "MCL1" :
+    [ "sorafenib" ],
+ "MET" :
+    [ "cabozantinib",
+      "crizotinib" ],
+ "MGMT" :
+    [ "carmustine",
+      "dacarbazine",
+      "temozolomide" ],
+ "MLK1" :
+    [ "osimertinib" ],
+ "MNK2" :
+    [ "osimertinib" ],
+ "MS4A1" :
+    [ "ibritumomab tiuxetan",
+      "obinutuzumab",
+      "ofatumumab",
+      "rituximab",
+      "tositumomab",
+      "ocrelizumab" ],
+ "MST1R" :
+    [ "crizotinib" ],
+ "MTNR1A" :
+    [ "tasimelteon" ],
+ "MTNR1B" :
+    [ "tasimelteon" ],
+ "MTOR" :
+    [ "everolimus",
+      "metformin",
+      "sirolimus",
+      "temsirolimus" ],
+ "NEK1" :
+    [ "dabrafenib" ],
+ "NEK11" :
+    [ "vemurafenib" ],
+ "NTRK1" :
+    [ "regorafenib" ],
+ "NTRK2" :
+    [ "cabozantinib" ],
+ "NUAK1" :
+    [ "brigatinib" ],
+ "Octreotide scan" :
+    [ "octreotide" ],
+ "Other" :
+    [ "bleomycin",
+      "carmustine",
+      "chlorambucil",
+      "cladribine",
+      "docetaxel",
+      "eribulin",
+      "gemcitabine",
+      "nelarabine",
+      "omacetaxine",
+      "paclitaxel" ],
+ "Others" :
+    [ "arsenic trioxide",
+      "asparaginase",
+      "busulfan",
+      "cabazitaxel",
+      "decitabine",
+      "digoxin",
+      "indomethacine",
+      "ingenol",
+      "pamidronate disodium",
+      "temozolomide",
+      "nelfinavir" ],
+ "PCSK9" :
+    [ "alirocumab",
+      "evolocumab" ],
+ "PDCD1" :
+    [ "nivolumab",
+      "pembrolizumab",
+      "avelumab" ],
+ "PDE4A" :
+    [ "apremilast" ],
+ "PDE4B" :
+    [ "apremilast" ],
+ "PDE4C" :
+    [ "apremilast" ],
+ "PDE4D" :
+    [ "apremilast" ],
+ "PDGFRA" :
+    [ "axitinib",
+      "ceritinib",
+      "ibrutinib",
+      "imatinib",
+      "lenvatinib",
+      "nilotinib",
+      "nintedanib",
+      "olaratumab",
+      "pazopanib",
+      "ponatinib",
+      "regorafenib",
+      "sunitinib" ],
+ "PDGFRB" :
+    [ "alectinib",
+      "axitinib",
+      "cabozantinib",
+      "dasatinib",
+      "imatinib",
+      "lenvatinib",
+      "nilotinib",
+      "nintedanib",
+      "pazopanib",
+      "ponatinib",
+      "regorafenib",
+      "sorafenib",
+      "sunitinib" ],
+ "PGR" :
+    [ "mifepristone",
+      "ulipristal" ],
+ "PHKG2" :
+    [ "brigatinib" ],
+ "PIK3C3" :
+    [ "idelalisib" ],
+ "PIK3CA" :
+    [ "idelalisib",
+      "copanlisib" ],
+ "PIK3CB" :
+    [ "idelalisib",
+      "copanlisib" ],
+ "PIK3CD" :
+    [ "idelalisib",
+      "copanlisib" ],
+ "PIK3CG" :
+    [ "idelalisib",
+      "copanlisib" ],
+ "PKCG" :
+    [ "midostaurin" ],
+ "PLCG1" :
+    [ "midostaurin" ],
+ "PLK1" :
+    [ "brigatinib" ],
+ "POLD1" :
+    [ "clofarabine" ],
+ "POLE" :
+    [ "clofarabine" ],
+ "POLx" :
+    [ "clofarabine" ],
+ "PPK" :
+    [ "midostaurin" ],
+ "PRKAA2" :
+    [ "axitinib" ],
+ "PRKACA" :
+    [ "midostaurin" ],
+ "PRKCA" :
+    [ "midostaurin" ],
+ "PRKCB" :
+    [ "midostaurin" ],
+ "PRKCB2" :
+    [ "midostaurin" ],
+ "PRKCG" :
+    [ "midostaurin" ],
+ "PRKCH" :
+    [ "midostaurin" ],
+ "PRKD1" :
+    [ "brigatinib",
+      "brigatinib" ],
+ "PRKD2" :
+    [ "brigatinib" ],
+ "PRKDC" :
+    [ "idelalisib" ],
+ "proteasome" :
+    [ "bortezomib",
+      "carfilzomib" ],
+ "PSMB1" :
+    [ "ixazomib" ],
+ "PSMB5" :
+    [ "ixazomib" ],
+ "PTGS1" :
+    [ "sulindac" ],
+ "PTGS2" :
+    [ "celecoxib",
+      "sulindac" ],
+ "PTK2" :
+    [ "brigatinib" ],
+ "PTK2B" :
+    [ "brigatinib" ],
+ "PTK5" :
+    [ "regorafenib" ],
+ "PTK6" :
+    [ "ibrutinib",
+      "vandetanib",
+      "vemurafenib",
+      "brigatinib" ],
+ "RAF1" :
+    [ "dabrafenib",
+      "regorafenib",
+      "sorafenib",
+      "vemurafenib" ],
+ "RET" :
+    [ "alectinib",
+      "axitinib",
+      "cabozantinib",
+      "ceritinib",
+      "ibrutinib",
+      "lenvatinib",
+      "ponatinib",
+      "regorafenib",
+      "sorafenib",
+      "sunitinib",
+      "vandetanib",
+      "brigatinib" ],
+ "RHOA" :
+    [ "zoledronic acid" ],
+ "RHOH" :
+    [ "zoledronic acid" ],
+ "RHOx" :
+    [ "zoledronic acid" ],
+ "RIPK2" :
+    [ "ibrutinib" ],
+ "RNDx" :
+    [ "zoledronic acid" ],
+ "ROCK2" :
+    [ "ceritinib" ],
+ "ROS1" :
+    [ "alectinib",
+      "ceritinib",
+      "crizotinib",
+      "cabozantinib",
+      "brigatinib" ],
+ "RPS6KA1" :
+    [ "brigatinib" ],
+ "RPS6KA3" :
+    [ "brigatinib" ],
+ "RPS6KA6" :
+    [ "brigatinib" ],
+ "RRM1" :
+    [ "gemcitabine" ],
+ "RRMx" :
+    [ "clofarabine" ],
+ "RXRA" :
+    [ "bexarotene" ],
+ "RXRB" :
+    [ "bexarotene" ],
+ "RXRG" :
+    [ "bexarotene" ],
+ "SAPK2" :
+    [ "regorafenib" ],
+ "SCFR" :
+    [ "imatinib" ],
+ "SGLT2" :
+    [ "dapagliflozin" ],
+ "SIK1" :
+    [ "dabrafenib" ],
+ "SIK2" :
+    [ "brigatinib" ],
+ "SLAMF7" :
+    [ "elotozumab" ],
+ "SLC29A1" :
+    [ "gemcitabine" ],
+ "SMO" :
+    [ "itraconazole",
+      "sonidegib",
+      "vismodegib" ],
+ "SPARC" :
+    [ "nab-paclitaxel" ],
+ "SRC" :
+    [ "bosutinib",
+      "dasatinib",
+      "ibrutinib",
+      "nintedanib",
+      "ponatinib",
+      "vandetanib",
+      "brigatinib",
+      "midostaurin" ],
+ "SRD5A1" :
+    [ "dutasteride" ],
+ "SRD5A2" :
+    [ "dutasteride" ],
+ "SRMS" :
+    [ "vemurafenib" ],
+ "STK10" :
+    [ "brigatinib" ],
+ "STK3" :
+    [ "axitinib" ],
+ "SYK" :
+    [ "midostaurin" ],
+ "TACR1" :
+    [ "aprepitant",
+      "rolapitant" ],
+ "TAOK1" :
+    [ "brigatinib" ],
+ "TEC" :
+    [ "ibrutinib" ],
+ "TEK" :
+    [ "cabozantinib",
+      "ponatinib",
+      "regorafenib",
+      "vandetanib" ],
+ "TGFB1" :
+    [ "pirfenidone" ],
+ "TLE3" :
+    [ "docetaxel",
+      "paclitaxel",
+      "nab-paclitaxel",
+      "cabazitaxel" ],
+ "TNF" :
+    [ "adalimumab",
+      "certolizumab pegol",
+      "golimumab",
+      "infliximab",
+      "infliximab-dyyb",
+      "lenalidomide",
+      "pomalidomide" ],
+ "TNFRSF11A" :
+    [ "pirfenidone" ],
+ "TNFRSF14" :
+    [ "pirfenidone" ],
+ "TNFRSF17" :
+    [ "pirfenidone" ],
+ "TNFRSF8" :
+    [ "brentuximab vedotin",
+      "pirfenidone" ],
+ "TNFSF11" :
+    [ "denosumab" ],
+ "TNFSF13B" :
+    [ "belimumab" ],
+ "TNK2" :
+    [ "osimertinib",
+      "vemurafenib" ],
+ "TOP1" :
+    [ "irinotecan",
+      "topotecan" ],
+ "TOP2A" :
+    [ "daunorubicin",
+      "doxorubicin",
+      "doxorubicin (liposomal)",
+      "epirubicin",
+      "etoposide",
+      "hygromycin",
+      "idarubicin",
+      "mitoxantrone",
+      "paclitaxel",
+      "teniposide",
+      "valrubicin" ],
+ "TOP2B" :
+    [ "daunorubicin",
+      "doxorubicin",
+      "doxorubicin (liposomal)",
+      "epirubicin",
+      "etoposide",
+      "idarubicin",
+      "mitoxantrone",
+      "teniposide",
+      "valrubicin" ],
+ "TRDMT1" :
+    [ "azacitidine" ],
+ "TSSK1B" :
+    [ "ceritinib",
+      "brigatinib" ],
+ "TSSK2" :
+    [ "brigatinib" ],
+ "TUBB3" :
+    [ "docetaxel",
+      "paclitaxel",
+      "nab-paclitaxel",
+      "cabazitaxel" ],
+ "TYK2" :
+    [ "ruxolitinib",
+      "tofacitinib" ],
+ "TYMP" :
+    [ "capecitabine",
+      "fluorouracil",
+      "pemetrexed",
+      "tipiracil-trifluridine" ],
+ "TYMS" :
+    [ "capecitabine",
+      "fluorouracil",
+      "pemetrexed" ],
+ "VEGFA" :
+    [ "bevacizumab",
+      "ranibizumab",
+      "ziv-aflibercept" ],
+ "VEGFB" :
+    [ "ziv-aflibercept" ],
+ "WNK3" :
+    [ "vemurafenib" ],
+ "YES1" :
+    [ "dasatinib",
+      "ibrutinib",
+      "ponatinib",
+      "vandetanib",
+      "vemurafenib",
+      "brigatinib"]
+]
