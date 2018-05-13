74758322d7623817eaa4a0e6b0f0eee069b8837c
Philippe Faurie <pfaurie@cox.net>
2018-05-12 19:07:49 -0700

    TsubData

---
+++ aOncology/Data/TSubData.swift
@@ -0,0 +1,465 @@
+var dicTSubsL : [String: [String: Int]] = 
+   [
+    "AKT1" :
+          [ "MTOR" : 1 ],
+    "AKT2" :
+          [ "MTOR" : 1 ],
+    "AKT3" :
+          [ "MTOR" : 1 ],
+    "AMER1" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "APC" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "ARAF" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "AREG" :
+          [ "EGFR" : 1 ],
+    "ARID1A" :
+          [ "MTOR" : 1,
+            "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "ATM" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "ATR" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "ATRX" :
+          [ "EZH2" : 1 ],
+    "AXIN1" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "BAP1" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "BARD1" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "BCOR" :
+          [ "BCL6" : 1 ],
+    "BCR" :
+          [ "BTK" : 1 ],
+    "BRAF" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "BRCA1" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "BRCA2" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "BRIP1" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "C11orf30" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "CCND1" :
+          [ "CDK4" : 1,
+            "CDK6" : 1,
+            "MTOR" : 1 ],
+    "CCND2" :
+          [ "CDK4" : 1,
+            "CDK6" : 1 ],
+    "CCND3" :
+          [ "CDK4" : 1,
+            "CDK6" : 1 ],
+    "CCNE1" :
+          [ "proteasome" : 1 ],
+    "CD274" :
+          [ "PDCD1" : 1 ],
+    "CDK12" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "CDK8" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "CDKN1A" :
+          [ "HDAC1" : 1 ],
+    "CDKN2A" :
+          [ "CDK4" : 1 ],
+    "CDKN2B" :
+          [ "CDK4" : 1 ],
+    "CRKL" :
+          [ "ABL1" : 1 ],
+    "CSF1" :
+          [ "CSF1R" : 1 ],
+    "CSF1R" :
+          [ "CSF1" : 1 ],
+    "CSF3" :
+          [ "CSF3R" : 1 ],
+    "CSF3R" :
+          [ "CSF3" : 1 ],
+    "CTNNA1" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "CTNNB1" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "CXCL12" :
+          [ "CXCR4" : 1,
+            "CXCR7" : 1 ],
+    "CXCR4" :
+          [ "CXCL12" : 1 ],
+    "CXCR7" :
+          [ "CXCL12" : 1 ],
+    "CYP17A1" :
+          [ "AR" : 1 ],
+    "CYP19A1" :
+          [ "ESR1" : 1 ],
+    "EMSY" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "ERBB3" :
+          [ "ERBB2" : 1 ],
+    "ERCC1" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "ERRFI1" :
+          [ "EGFR" : 1,
+            "ERBB2" : 1 ],
+    "EWSR1" :
+          [ "IGF1R" : 1 ],
+    "FANCA" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "FANCC" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "FANCD2" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "FANCE" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "FANCF" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "FANCG" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "FANCL" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "FAT1" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "FBXW7" :
+          [ "MTOR" : 1 ],
+    "FGF1" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1,
+            "FGFR5" : 1 ],
+    "FGF10" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1 ],
+    "FGF15" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF16" :
+          [ "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF17" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF18" :
+          [ "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF19" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF2" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1,
+            "FGFR5" : 1 ],
+    "FGF20" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF21" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF22" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1 ],
+    "FGF23" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF3" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1 ],
+    "FGF4" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF5" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1 ],
+    "FGF6" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR4" : 1 ],
+    "FGF7" :
+          [ "FGFR2" : 1,
+            "FGFR4" : 1 ],
+    "FGF8" :
+          [ "FGFR1" : 1,
+            "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "FGF9" :
+          [ "FGFR2" : 1,
+            "FGFR3" : 1,
+            "FGFR4" : 1 ],
+    "GNA11" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "GNAQ" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "GNAS" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "GNRHR" :
+          [ "AR" : 1,
+            "ESR1" : 1 ],
+    "H3F3A" :
+          [ "EZH2" : 1,
+            "HDAC1" : 1 ],
+    "HGF" :
+          [ "MET" : 1 ],
+    "HRAS" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "IDH1" :
+          [ "DNMT1" : 1 ],
+    "IDH2" :
+          [ "DNMT1" : 1 ],
+    "IGF1" :
+          [ "IGF1R" : 1,
+            "INSR" : 1 ],
+    "IGF2" :
+          [ "IGF1R" : 1,
+            "IGF2R" : 1,
+            "INSR" : 1 ],
+    "IL12B" :
+          [ "IL12RB1" : 1,
+            "IL12RB2" : 1,
+            "IL23R" : 1 ],
+    "IL17RA" :
+          [ "IL17A" : 1 ],
+    "IL17RB" :
+          [ "IL17A" : 1 ],
+    "IL17RC" :
+          [ "IL17A" : 1 ],
+    "IL17RD" :
+          [ "IL17A" : 1 ],
+    "IL17RE" :
+          [ "IL17A" : 1 ],
+    "IL17REL" :
+          [ "IL17A" : 1 ],
+    "IL3" :
+          [ "IL3RA" : 1 ],
+    "IL6" :
+          [ "IL17A" : 1 ],
+    "INPP4B" :
+          [ "MTOR" : 1 ],
+    "INS" :
+          [ "IGF1R" : 1,
+            "INSR" : 1 ],
+    "IRS2" :
+          [ "MTOR" : 1 ],
+    "KDR" :
+          [ "KDR" : 1 ],
+    "KRAS" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "MAPK1" :
+          [ "EGFR" : 1 ],
+    "MEN1" :
+          [ "SMO" : 1 ],
+    "MIF" :
+          [ "CXCR2" : 1,
+            "CXCR4" : 1 ],
+    "MUTYH" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "MYC" :
+          [ "CDK4" : 1 ],
+    "MYD88" :
+          [ "BTK" : 1 ],
+    "NF1" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1,
+            "MTOR" : 1 ],
+    "NF2" :
+          [ "MTOR" : 1 ],
+    "NRAS" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "PALB2" :
+          [ "DNA damage" : 1,
+            "PARP1" : 1,
+            "PARP2" : 1 ],
+    "PARP1" :
+          [ "DNA damage" : 1 ],
+    "PARP2" :
+          [ "DNA damage" : 1 ],
+    "PDCD1" :
+          [ "CD274" : 1,
+            "PDCD1LG2" : 1 ],
+    "PDCD1LG2" :
+          [ "PDCD1" : 1 ],
+    "PDGFA" :
+          [ "PDGFRA" : 1,
+            "PDGFRB" : 1 ],
+    "PDGFB" :
+          [ "PDGFRA" : 1,
+            "PDGFRB" : 1 ],
+    "PDGFC" :
+          [ "PDGFRA" : 1,
+            "PDGFRB" : 1 ],
+    "PDGFD" :
+          [ "PDGFRA" : 1,
+            "PDGFRB" : 1 ],
+    "PIK3C2B" :
+          [ "MTOR" : 1 ],
+    "PIK3CA" :
+          [ "MTOR" : 1 ],
+    "PIK3CB" :
+          [ "MTOR" : 1 ],
+    "PIK3CG" :
+          [ "MTOR" : 1 ],
+    "PIK3R1" :
+          [ "MTOR" : 1,
+            "PIK3CA" : 1 ],
+    "PIK3R2" :
+          [ "MTOR" : 1,
+            "PIK3CA" : 1 ],
+    "PTCH1" :
+          [ "SMO" : 1 ],
+    "PTEN" :
+          [ "MTOR" : 1,
+            "PIK3CA" : 1 ],
+    "PTGS2" :
+          [ "IL17A" : 1 ],
+    "PTPN11" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "PTPRD" :
+          [ "JAK1" : 1,
+            "JAK2" : 1,
+            "JAK3" : 1 ],
+    "RAF1" :
+          [ "MAP2K1" : 1,
+            "MAP2K2" : 1 ],
+    "RNF43" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "RPTOR" :
+          [ "MTOR" : 1 ],
+    "RSPO1" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "SHH" :
+          [ "SMO" : 1 ],
+    "SLIT2" :
+          [ "PTGS1" : 1,
+            "PTGS2" : 1 ],
+    "STAT3" :
+          [ "JAK1" : 1,
+            "JAK2" : 1,
+            "JAK3" : 1 ],
+    "STK11" :
+          [ "MTOR" : 1 ],
+    "TERT" :
+          [ "PARP1" : 1,
+            "PARP2" : 1 ],
+    "TET1" :
+          [ "DNMT1" : 1 ],
+    "TET2" :
+          [ "DNMT1" : 1 ],
+    "TGFB1" :
+          [ "TGFBR1" : 1,
+            "TGFBR2" : 1,
+            "TGFBR3" : 1 ],
+    "TGFB2" :
+          [ "TGFBR1" : 1,
+            "TGFBR2" : 1,
+            "TGFBR3" : 1 ],
+    "TGFB3" :
+          [ "TGFBR1" : 1,
+            "TGFBR2" : 1,
+            "TGFBR3" : 1 ],
+    "TGFBR1" :
+          [ "TGFB1" : 1 ],
+    "TGFBR2" :
+          [ "TGFB1" : 1 ],
+    "TP53" :
+          [ "FLT1" : 1,
+            "KDR" : 1 ],
+    "TSC1" :
+          [ "MTOR" : 1 ],
+    "TSC2" :
+          [ "MTOR" : 1 ],
+    "VEGFA" :
+          [ "FLT1" : 1,
+            "KDR" : 1 ],
+    "VEGFB" :
+          [ "FLT1" : 1,
+            "NRP1" : 1 ],
+    "VEGFC" :
+          [ "FLT4" : 1,
+            "KDR" : 1,
+            "NRP1" : 1,
+            "NRP2" : 1 ],
+    "VEGFD" :
+          [ "FLT4" : 1,
+            "KDR" : 1,
+            "NRP1" : 1,
+            "NRP2" : 1 ],
+    "VHL" :
+          [ "FLT1" : 1,
+            "KDR" : 1 ]
+   ]
