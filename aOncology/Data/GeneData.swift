eab22f9dd6fd551aafc73d1f571660c7fee5a3d9
Philippe Faurie <pfaurie@cox.net>
2018-05-12 19:06:57 -0700

    Genedata

---
+++ aOncology/Data/GeneData.swift
@@ -0,0 +1,20 @@
+//
+//  GeneData.swift
+//  aOncology
+//
+//  Created by Philippe-Faurie on 5/8/18.
+//  Copyright © 2018 Philippe-Faurie. All rights reserved.
+//
+
+import Foundation
+
+var list1 = Set(dicDTRelL.keys)                // Target - Drugs
+var list2 = Set(dicTSubsL.keys)                // Target - Target
+var synGene = Array (synoGeneData.keys)
+
+//var geneDataList = synGene                        // All synonyms
+//var geneDataList = list1.union(list2)           // All
+
+var geneDataList = Array (list1.union(list2)) + synGene                       // All synonyms
+
+
