e6aa95512449a47eed2e1d7dae9acdd11a261f3a
Philippe Faurie <pfaurie@cox.net>
2018-05-18 18:13:32 -0700

    1 file

---
+++ aOncology/Data/PathoSynData.swift
@@ -0,0 +1,330 @@
+ var pathoSynData = [ 
+
+  "Acute Lymphoblastic Leukemia ": [
+            "Acute Lymphoblastic Leukemia",
+            "acute lymphoblastic",
+            "ALL"],
+
+  "Acute Myeloid Leukemia": [
+            "Acute Myeloid Leukemia",
+            "acute myeloblastic",
+            "Acute myeloid",
+            "Acute nonlymphocytic",
+            "Acute promyelocytic",
+            "AML"],
+
+  "AIDS-Related Kaposi Sarcoma": [
+            "AIDS-Related Kaposi Sarcoma",
+            "kaposi",
+            "Kaposi's sarcomas"],
+
+  "Anal Carcinoma": [
+            "anal",
+            "anus",
+            "Anal Carcinoma"],
+
+  "Basal Cell Skin Cancer": [
+            "basal",
+            "Basal Cell Skin Cancer"],
+
+  "Bladder Cancer": [
+            "bladder",
+            "urachal",
+            "urethra",
+            "urinary",
+            "urothelial",
+            "Bladder Cancer"],
+
+  "Bone Cancer": [
+            "bone",
+            "chondrosarcoma",
+            "chordoma",
+            "Ewing",
+            "osteosarcoma",
+            "pleomorphic sarcoma",
+            "Bone Cancer"],
+
+  "Breast Cancer": [
+            "breast",
+            "mammary",
+            "Breast Cancer"],
+
+  "Central Nervous System Cancers": [
+            "astrocytoma",
+            "brain",
+            "CNS",
+            "ependymoma",
+            "glioblastoma",
+            "glioma",
+            "Leptomeningeal",
+            "medulloblastoma",
+            "meningeal",
+            "meningioma",
+            "nervous",
+            "neuroblastoma",
+            "Oligodendroglioma",
+            "Retinoblastoma",
+            "supratentorial",
+            "tentorial",
+            "Central Nervous System Cancers"],
+
+  "Cervical Cancer": [
+            "cervical",
+            "cervix",
+            "choriocarcinoma",
+            "endometre",
+            "endometrial",
+            "endometrium",
+            "trophoblastic",
+            "uterine",
+            "uterus",
+            "Cervical Cancer"],
+
+  "Chronic Lymphocytic Leukemia": [
+            "chronic lymphocytic",
+            "CLL",
+            "SLL",
+            "Small lymphocytic",
+            "Chronic Lymphocytic Leukemia"],
+
+  "Chronic Myeloid Leukemia": [
+            "chronic eosinophilic",
+            "chronic myelocytic",
+            "chronic myelogenous",
+            "CML",
+            "Chronic Myeloid Leukemia"],
+
+  "Colon Cancer": [
+            "colon",
+            "colorectal",
+            "colorectum",
+            "duodenal",
+            "duodenum",
+            "Colon Cancer"],
+
+  "Esophageal Cancers": [
+            "esophageal",
+            "esophagogastric",
+            "esophagus",
+            "Esophageal Cancers"],
+
+  "Gastric Cancer": [
+            "gastric",
+            "stomach",
+            "gastrointestinal stromal",
+            "GIST",
+            "Gastric Cancer"],
+
+  "Hairy Cell Leukemia": [
+            "Hairy Cell Leukemia"],
+
+  "Head and Neck Cancers": [
+            "ethmoid",
+            "glottic",
+            "head",
+            "hypopharynx",
+            "larynx",
+            "lip",
+            "maxillary",
+            "nasopharynx",
+            "neck",
+            "oral",
+            "oral cavity",
+            "oropharynx",
+            "parathyroid",
+            "parotid",
+            "pharynx",
+            "sinus",
+            "supraglottic",
+            "Head and Neck Cancers"],
+
+  "Hepatobiliary Cancers": [
+            "biliary",
+            "cholangiocarcinoma",
+            "extrahepatic",
+            "gallbladder",
+            "hepatic",
+            "hepatobiliary",
+            "intrahepatic",
+            "pancreatobiliary",
+            "hepatoblastoma",
+            "hepatocarcinoma",
+            "hepatocellular",
+            "hepatoma",
+            "liver",
+            "Hepatobiliary Cancers"],
+
+  "Hodgkin Lymphoma": [
+            "Hodgkin",
+            "Hodgkin's",
+            "Hodgkin Lymphoma"],
+
+  "Kidney Cancer": [
+            "kidney",
+            "nephroblatoma",
+            "nephros",
+            "renal",
+            "wilm",
+            "Kidney Cancer"],
+
+  "Malignant Pleural Mesothelioma": [
+            "mesothelioma",
+            "Malignant Pleural Mesothelioma"],
+
+  "Melanoma": [
+            "Melanoma"],
+
+  "Merkel Cell Carcinoma": [
+            "merkel",
+            "Merkel Cell Carcinoma"],
+
+  "Non-Small Cell Lung Cancer": [
+            "bronchogenic",
+            "NSCLC",
+            "pleural",
+            "SCLC",
+            "Non-Small Cell Lung Cancer",
+            "Lung Cancer"],
+
+  "Multiple Myeloma ": [
+            "Amyloidosis",
+            "myeloma",
+            "Multiple Myeloma "],
+
+  "Myelodysplastic Syndromes": [
+            "MDS",
+            "Myelodysplastic",
+            "Polycythemia",
+            "Myelodysplastic Syndromes"],
+
+  "Myeloid Growth Factors": [
+            "Myeloid Growth Factors"],
+
+  "Myeloproliferative Neoplasms - Myelofibrosis": [
+            "Myelofibrosis"],
+
+  "Neuroendocrine and Adrenal Tumor": [
+            "acromegaly",
+            "carcinoid",
+            "neuroendocrine",
+            "paraganglioma",
+            "peptide",
+            "pheochromocytoma",
+            "VIP",
+            "appendiceal",
+            "appendix",
+            "Neuroendocrine and Adrenal Tumor"],
+
+  "Occult Primary": [
+            "Occult Primary"],
+
+  "Ovarian Cancer": [
+            "fallopian",
+            "ovarian",
+            "ovary",
+            "Ovarian Cancer"],
+
+  "Pancreatic Adenocarcinoma": [
+            "pancreas",
+            "pancreatic",
+            "pancreatobiliary",
+            "Pancreatic Adenocarcinoma"],
+
+  "Penile Cancer": [
+            "penile",
+            "penis",
+            "Penile Cancer"],
+
+  "Prostate Cancer ": [
+            "prostate",
+            "prostatic",
+            "Prostate Cancer "],
+
+  "Rectal Cancer": [
+            "rectal",
+            "rectum",
+            "Rectal Cancer"],
+
+  "B-Cell Lymphomas": [
+            "B-Cell Lymphomas",
+            "Gastric MALT Lymphoma",
+            "Mantle Cell Lymphoma",
+            "Nongastric MALT Lymphoma",
+            "Castleman's Disease",
+            "Burkitt Lymphoma"],
+
+  "T-Cell Lymphomas": [
+            "non-hodgkin's",
+            "non-hodgkins",
+            "non-hodgkin",
+            "Burkitt",
+            "Castelman",
+            "chronic lymphocytic",
+            "CLL",
+            "fungoides",
+            "histiocytosis",
+            "Lymphoplasmacytic",
+            "lymphoproliferative",
+            "lymphosarcoma",
+            "Macroglobulinemia",
+            "MF",
+            "mycosis fungoides",
+            "NHL",
+            "Prolymphocytic",
+            "Sezary",
+            "SLL",
+            "Small Lymphocytic",
+            "SS",
+            "Waldenstrom",
+            "non-hodgkin lymphoma",
+            "T-Cell Lymphomas"],
+
+  "Small intestine tumors": [
+            "bowel",
+            "intestinal",
+            "intestine"],
+
+  "Soft Tissue Sarcoma": [
+            "angiomyolipoma",
+            "angiosarcoma",
+            "desmoid",
+            "fibromatosis",
+            "Hemangiopericytoma",
+            "leiomyomatosis",
+            "leiomyosarcoma",
+            "liposarcoma",
+            "Lymphangioleiomyomatosis",
+            "myofibroblastic",
+            "myxoid",
+            "pleomorphic sarcoma",
+            "Rhabdomyosarcoma",
+            "soft",
+            "stromal",
+            "Soft Tissue Sarcoma"],
+
+  "Squamous Cell Skin Cancer": [
+            "squamous",
+            "dermatofibrosarcoma",
+            "keratosis",
+            "merkel",
+            "skin",
+            "Squamous Cell Skin Cancer"],
+
+  "Testicular Cancer": [
+            "testicular",
+            "testicule",
+            "Testicular Cancer"],
+
+  "Thymomas and Thymic Carcinomas": [
+            "thymoma",
+            "thymus",
+            "hurthle"],
+
+  "Thyroid Carcinoma": [
+            "Thyroid Carcinoma",
+            "thyroid"],
+
+  "Vulvar Cancer": [
+            "Vulva",
+            "Vulvar Cancer"] 
+]
